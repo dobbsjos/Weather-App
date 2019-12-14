3737a706d3c85072d40e3efe0b94d8b47dd8fbcd
Joshua Dobbs <dobbsjos@gmail.com>
2019-12-14 13:56:22 -0500

    broken button

--- weather.js
+++ weather.js
@@ -4,23 +4,40 @@ $(document).ready(function () {
     var APIKey = "1d6d7d616cf0e8e67fe23fcb5edfbdb5";
 
     // Here we are building the URL we need to query the database
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-        "q=Orlando&units=imperial&appid=" + APIKey;
-
+    var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" +
+        searchValue + "&units=imperial&appid=" + APIKey;
+    var searchValue = $('#search-value').value()
     // Here we run our AJAX call to the OpenWeatherMap API
-    $.ajax({
-        url: queryURL,
-        method: "GET"
+    // $.ajax({
+    //     url: weatherUrl,
+    //     method: "GET"
+    // })
+    //     // We store all of the retrieved data inside of an object called "response"
+    //     .then(function (response) {
+
+    //         // Log the queryURL
+    //         console.log(weatherUrl);
+
+    //         // Log the resulting object
+    //         console.log(response);
+    //     })
+
+    function currentWeather() {
+        $.ajax({
+            url: weatherUrl,
+            method: 'GET',
+        }).then(function (response) {
+            console.log(weatherUrl)
+            console.log(response)
+
+        });
+    }
+
+    $('#search-button').on('click', function () {
+        var searchValue = $('#search-value').value()
+
+        currentWeather(searchValue);
     })
-        // We store all of the retrieved data inside of an object called "response"
-        .then(function (response) {
-
-            // Log the queryURL
-            console.log(queryURL);
-
-            // Log the resulting object
-            console.log(response);
-        })
 
     // var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=orlando1d6d7d616cf0e8e67fe23fcb5edfbdb5"
     // var weatherApiKey = "1d6d7d616cf0e8e67fe23fcb5edfbdb5"
@@ -33,21 +50,5 @@ $(document).ready(function () {
     //     console.log(response)
     // })
 
-    //     function currentWeather() {
-    //         $.ajax({
-    //             url: weatherUrl + searchValue + weatherApiKey,
-    //             method: 'GET',
-    //         }).then(function (response) {
-    //             console.log(url)
-    //             console.log(response)
 
-    //             $('.today').html('<h1>' + response.name + 'Weather Details</h1>' + "Temp")
-    //         });
-    //     }
-
-    // $('#search-button').on('click', function () {
-    //     var searchValue = $('#search-value').value()
-
-    //     currentWeather(searchValue);
-    // })
 })
