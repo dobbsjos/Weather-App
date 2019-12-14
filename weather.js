bc87585190a8d0c3a0b108e0aa08a780ff4ad6d3
Joshua Dobbs <dobbsjos@gmail.com>
2019-12-14 14:16:14 -0500

    second button still not working

--- weather.js
+++ weather.js
@@ -6,23 +6,9 @@ $(document).ready(function () {
     // Here we are building the URL we need to query the database
     var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" +
         searchValue + "&units=imperial&appid=" + APIKey;
-    var searchValue = $('#search-value').value()
-    // Here we run our AJAX call to the OpenWeatherMap API
-    // $.ajax({
-    //     url: weatherUrl,
-    //     method: "GET"
-    // })
-    //     // We store all of the retrieved data inside of an object called "response"
-    //     .then(function (response) {
+    var searchValue = $('#search-value').val()
 
-    //         // Log the queryURL
-    //         console.log(weatherUrl);
-
-    //         // Log the resulting object
-    //         console.log(response);
-    //     })
-
-    function currentWeather() {
+    function currentWeather(event) {
         $.ajax({
             url: weatherUrl,
             method: 'GET',
@@ -38,17 +24,4 @@ $(document).ready(function () {
 
         currentWeather(searchValue);
     })
-
-    // var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=orlando1d6d7d616cf0e8e67fe23fcb5edfbdb5"
-    // var weatherApiKey = "1d6d7d616cf0e8e67fe23fcb5edfbdb5"
-
-    // $.ajax({
-    //     url: weatherUrl,
-    //     method: 'GET'
-    // }).then(function(response){
-    //     console.log(url)
-    //     console.log(response)
-    // })
-
-
 })
