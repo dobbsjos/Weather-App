4256ae6f2723828d469e2a6642d61ebad87f3c58
Joshua Dobbs <dobbsjos@gmail.com>
2019-12-14 14:35:21 -0500

    another failed button

--- weather.js
+++ weather.js
@@ -6,22 +6,32 @@ $(document).ready(function () {
     // Here we are building the URL we need to query the database
     var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" +
         searchValue + "&units=imperial&appid=" + APIKey;
-    var searchValue = $('#search-value').val()
+    var searchValue = document.getElementById("#searchValue")
+    console.log(searchValue);
 
-    function currentWeather(event) {
+    $("#search-value").on("click", function(event) {
+
+        // event.preventDefault() can be used to prevent an event's default behavior.
+        // Here, it prevents the submit button from trying to submit a form when clicked
+        event.preventDefault();
+
+        // Here we grab the text from the input box
+        // var movie = $("#movie-input").val();
+
+        // Here we construct our URL
+        // var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+
+        // Write code between the dashes below to hit the queryURL with $ajax, then take the response data
+        // and display it in the div with an id of movie-view
+
+        // ------YOUR CODE GOES IN THESE DASHES. DO NOT MANUALLY EDIT THE HTML ABOVE.
+
         $.ajax({
-            url: weatherUrl,
-            method: 'GET',
-        }).then(function (response) {
-            console.log(weatherUrl)
-            console.log(response)
-
+          url: weatherUrl,
+          method: "GET"
+        }).then(function(response) {
+          $("#search-value").text(JSON.stringify(response));
         });
-    }
-
-    $('#search-button').on('click', function () {
-        var searchValue = $('#search-value').value()
-
-        currentWeather(searchValue);
-    })
+    
+})
 })
