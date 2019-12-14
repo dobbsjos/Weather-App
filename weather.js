4887967467ce3fe8939acb3cb17e9cce11dd9cec
Joshua Dobbs <dobbsjos@gmail.com>
2019-12-14 15:10:57 -0500

    finally a working button

--- weather.js
+++ weather.js
@@ -1,22 +1,23 @@
 $(document).ready(function () {
 
     // This is our API key
-    var APIKey = "1d6d7d616cf0e8e67fe23fcb5edfbdb5";
+
 
     // Here we are building the URL we need to query the database
-    var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" +
-        searchValue + "&units=imperial&appid=" + APIKey;
-    var searchValue = document.getElementById("#searchValue")
-    console.log(searchValue);
 
-    $("#search-value").on("click", function(event) {
+    // var searchValue = $('#search-value').val()
+    // var searchValue = document.getElementById("#searchValue")
+    // console.log(searchValue);
+    var APIKey = "1d6d7d616cf0e8e67fe23fcb5edfbdb5";
+    
+    $("#search-button").on("click", function (event) {
 
         // event.preventDefault() can be used to prevent an event's default behavior.
         // Here, it prevents the submit button from trying to submit a form when clicked
         event.preventDefault();
+        var searchValue = $('#search-value').val();
+        var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&units=imperial&appid=" + APIKey;
 
-        // Here we grab the text from the input box
-        // var movie = $("#movie-input").val();
 
         // Here we construct our URL
         // var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
@@ -27,11 +28,12 @@ $(document).ready(function () {
         // ------YOUR CODE GOES IN THESE DASHES. DO NOT MANUALLY EDIT THE HTML ABOVE.
 
         $.ajax({
-          url: weatherUrl,
-          method: "GET"
-        }).then(function(response) {
-          $("#search-value").text(JSON.stringify(response));
+            url: weatherUrl,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            console.log(weatherUrl)
         });
-    
-})
+
+    })
 })
